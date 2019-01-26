a70d4c53710703ed54bcd7dda5779ceb54b33baa
Victor Inojosa <vijoin@gmail.com>
2019-01-26 08:59:53 -0400

    Chapter 5 > Defining the Model representation and order

--- my_module/models/library_book.py
+++ my_module/models/library_book.py
@@ -5,8 +5,12 @@ from odoo import fields, models
 
 class LibraryBook(models.Model):
     _name = 'library.book'
+    _description = 'Library Book'
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name'
 
     name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Name', required=True)
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many(
         'res.partner',
