bb32843b3d598cfb4ad202a20054c7df3bf891d4
Victor Inojosa <vijoin@gmail.com>
2019-01-28 08:29:06 -0400

    Chapter 5: Adding data fields to a Model

--- my_module/models/library_book.py
+++ my_module/models/library_book.py
@@ -10,12 +10,44 @@ class LibraryBook(models.Model):
     _rec_name = 'short_name'
 
     name = fields.Char('Title', required=True)
-    short_name = fields.Char('Short Name', required=True)
+    short_name = fields.Char('Short Title',
+                             required=True,
+                             size=100,
+                             translate=False)
+    notes = fields.Text('Internatl Notes')
+    state = fields.Selection(
+        [('draft','Not Available'),
+         ('available','Available'),
+         ('lost','Lost')],
+        'State')
+    description = fields.Html(string='Description',
+                              sanitize=True,
+                              strip_style=True,
+                              translate=False,)
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
     date_release = fields.Date('Release Date')
+    date_update = fields.Datetime('Last Updated')
+    pages = fields.Integer('Number of Pages',
+                           default=0,
+                           help='Total book page count',
+                           groups='base.group_user',
+                           states={'lost': [('readonly', True)]},
+                           copy=True,
+                           index=False,
+                           readonly=False,
+                           required=False,
+                           company_dependent=False,
+                           )
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        digits=(14,4),
+    )
     author_ids = fields.Many2many(
         'res.partner',
         string = 'Authors'
     )
+    active = fields.Boolean('Active')
 
     def name_get(self):
         result = []
