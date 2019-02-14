5079d99e613c88ecdbb847f70d5560c142fdd698
Victor Inojosa <vijoin@gmail.com>
2019-02-14 09:26:09 -0400

    Chapter 5 > Using abstract Models for reusable Model features

--- my_module/models/library_book.py
+++ my_module/models/library_book.py
@@ -4,9 +4,19 @@ from odoo import fields, models, api
 from odoo.addons import decimal_precision as dp
 from odoo.fields import Date as fDate
 
+class BaseArchive(models.AbstractModel):
+    _name = 'base.archive'
+
+    active = fields.Boolean(default=True)
+
+    def do_archive(self):
+        for record in self:
+            record.active = not record.active
+
 
 class LibraryBook(models.Model):
     _name = 'library.book'
+    _inherit = ['base.archive']
     _description = 'Library Book'
     _order = 'date_release desc, name'
     _rec_name = 'short_name'
